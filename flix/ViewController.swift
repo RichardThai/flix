fe4b7616fc9dc497839b65bace3a585e60f2118c
Richard Thai <Richard.Thai@icloud.com>
2019-10-18 14:58:34 -0700

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Richard Thai on 10/18/19.
+//  Copyright © 2019 richard.thai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
